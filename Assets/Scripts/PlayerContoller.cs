d9c84f0edea905714183e93e6be8f325e0fc0330
blazetbd <blazetbd@gmail.com>
2025-08-24 16:17:55 -0400

    Add player controls and wax levels

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -1,16 +1,70 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerContoller : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public float horizontalInput;
+    public float jumpForce = 5.0f;
+    public float waxLevel = 100.0f;
+    private float waxLimit = 100.0f;
+    public float waxTickRate = -1.0f;
+    private Rigidbody2D playerRb;
+    private bool isOnGround = true;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("WaxTick", 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector2.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("WaxPile"))
+        {
+            waxTickRate = 1.0f;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("WaxPile"))
+        {
+            waxTickRate = -1.0f;
+        }
+    }
+
+    private void WaxTick()
+    {
+        if (waxTickRate > 0)
+        {
+            waxLevel = Mathf.Min(waxLevel + waxTickRate, waxLimit);
+        }
+        else
+        {
+            waxLevel = Mathf.Max(waxLevel + waxTickRate, 0);
+        }
     }
 }
